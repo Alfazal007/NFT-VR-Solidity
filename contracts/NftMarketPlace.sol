beba8b87e5841d83f86be32acbd6eaef19c78e5f
Alfazal007 <alfazal7777@gmail.com>
2024-02-27 23:48:43 +0530

    exchange nft done

--- contracts/NftMarketPlace.sol
+++ contracts/NftMarketPlace.sol
@@ -1,48 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 error NftMarketPlace__NotOwner();
 error NftMarketPlace__InsufficientAmount();
 error NftMarketPlace__InsufficientPriceSet();
+error NftMarketPlace__NowOwner();
+error NftMarketPlace__InsufficientAmountSent();
+error NftMarketPlace__NotForSale();
+error NftMarketPlace__ExchangeFailed();
 
 contract NftMarketPlace is ERC721URIStorage {
-
-    event MarketItemCreated(uint256 tokenId,
+    // EVENTS
+    event MarketItemCreated(
+        uint256 tokenId,
         address indexed creator,
         address indexed approvedOwner,
         uint256 indexed price,
         bool sold
-        );
+    );
 
-    // initial storage variables to be initialized in the constructor
+    // VARIABLES
     address private immutable i_owner;
     uint256 private currentTokenId;
     uint256 private s_soldNfts;
     uint256 private listingPrice;
-    
-    // constructor to initialize local variables and NFT collections
-    constructor() ERC721("VR Art Tokens", "ENMA") {
-        i_owner = msg.sender;
-        currentTokenId = 0;
-        s_soldNfts = 0;
-        listingPrice = 0.02 ether;
-    }
+    mapping(uint256 => MarketItem) tokenIdToMarketItem;
 
-
-    modifier onlyOwner {
-        if(msg.sender != i_owner) {
+    // MODIFIER
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
             revert NftMarketPlace__NotOwner();
         }
         _;
     }
 
-
-
-    // the structure array to be maintained
+    // STRUCT
     struct MarketItem {
         uint256 tokenId; // id of the token
         address seller; // seller of the token (initially the minter)
@@ -51,15 +46,25 @@ contract NftMarketPlace is ERC721URIStorage {
         bool sold; // sold or not
     }
 
-    mapping (uint256 => MarketItem) tokenIdToMarketItem; // only the listed items present here
+    // CONSTRUCTOR
+    constructor() ERC721("VR Art Tokens", "ENMA") {
+        i_owner = msg.sender;
+        currentTokenId = 1;
+        s_soldNfts = 0;
+        listingPrice = 0.02 ether;
+    }
 
+    // MAIN FUNCTIONS
 
-    // Mint a token and lists it in the marketplace -- _safeMint setTokenURI 
-    function createNft(uint256 price, string memory tokenUri) public payable returns(uint256) {
-        if(msg.value < listingPrice) {
+    // CREATE NFT AND LIST INTO MARKETPLACE
+    function createNft(
+        uint256 price,
+        string memory tokenUri
+    ) external payable returns (uint256) {
+        if (msg.value < listingPrice) {
             revert NftMarketPlace__InsufficientAmount();
         }
-        if(price <= 0) {
+        if (price <= 0) {
             revert NftMarketPlace__InsufficientPriceSet();
         }
         _safeMint(msg.sender, currentTokenId); // create the token
@@ -69,51 +74,71 @@ contract NftMarketPlace is ERC721URIStorage {
         return currentTokenId - 1;
     }
 
-    // createMarketItem private function to be called from above after creation to be listed and also approval to sell the NFT
-    function listItemToMarketPlace(uint256 tokenId, uint256 price, address seller) private {
-        if(msg.value < listingPrice) {
-            revert NftMarketPlace__InsufficientAmount();
-        }
-        if(price <= 0) {
+    // LIST THE NFT INTO THE MARKETPLACE
+    function listItemToMarketPlace(
+        uint256 tokenId,
+        uint256 price,
+        address seller
+    ) private {
+        if (price <= 0) {
             revert NftMarketPlace__InsufficientPriceSet();
         }
         tokenIdToMarketItem[tokenId] = MarketItem(
             tokenId,
-            seller, // creator
-            address(this), // the contract to transfer the ownership
+            seller, // THE ONE WHO CALLED MINT FUNCTION OR RELIST FUNCTION
+            address(this), // SET TO THE CONTRACT ADDRESS
             price,
             false
         );
-        approve(address(this), tokenId);
+        approve(address(this), tokenId); // APPROVED TO BE SOLD
         emit MarketItemCreated(
-        tokenId,
-        msg.sender,
-        address(this),
-        price,
-        false
+            tokenId,
+            msg.sender,
+            address(this),
+            price,
+            false
         );
     }
-    // allows someone to resell a token they have purchased manipulate the number of tokens sold and relist it after changing the ownership getting the price and approval
 
+    function relistNft(uint256 tokenId, uint256 price) external payable {}
 
     // Creates the sale of a marketplace item exchange of money and ownership of the listed NFT
+    function exchangeNft(uint256 tokenId) external payable {
+        MarketItem memory curItem = tokenIdToMarketItem[tokenId];
+        if (curItem.sold == true) {
+            revert NftMarketPlace__NotForSale();
+        }
+        if (msg.value < curItem.price) {
+            revert NftMarketPlace__InsufficientAmountSent();
+        }
+        tokenIdToMarketItem[tokenId].sold = true;
+        safeTransferFrom(curItem.seller, msg.sender, tokenId);
+        s_soldNfts = s_soldNfts + 1;
+        address previousOwner = curItem.seller;
+        tokenIdToMarketItem[tokenId].seller = msg.sender;
+        tokenIdToMarketItem[tokenId].owner = address(0x0);
+        (bool callSuccess1, ) = payable(i_owner).call{value: listingPrice}("");
+        if (callSuccess1 == false) {
+            revert NftMarketPlace__ExchangeFailed();
+        }
+        (bool callSuccess2, ) = payable(previousOwner).call{value: msg.value}(
+            ""
+        );
+        if (callSuccess2 == false) {
+            revert NftMarketPlace__ExchangeFailed();
+        }
+    }
 
     // Return all unsold market items (to be listed) only if it is listed to be sold
     // Return only items that a user has purchased (my purchase section)
     // Return only items a user has listed (my to be sold section)
-    
-    
-    
-    
-    
-    
-    
-    function getListingPrice() public view returns(uint256) {
+
+    function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
 
     function setListingPrice(uint256 price) public onlyOwner {
-        if(price <= 0) {
+        if (price <= 0) {
             revert NftMarketPlace__InsufficientAmount();
         }
         listingPrice = price;
